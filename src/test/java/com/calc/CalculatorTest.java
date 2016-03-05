778dc8ee3ced3940e2bca05a2738a063dca755c3
dougwyllie <dougwyllie@shaw.ca>
2016-03-05 01:23:29 -0700

    Updated Comments

--- src/test/java/com/calc/CalculatorTest.java
+++ src/test/java/com/calc/CalculatorTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit tests for the Calculator program.
  */
 public class CalculatorTest extends TestCase
 {
